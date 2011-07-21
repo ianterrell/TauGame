cb477abf98df8f36020a777448f67f576dd81610
Ian Terrell <ian.terrell@gmail.com>
2011-07-21 16:23:41 -0400

    added a few baddie animations

--- TauGame/AsteroidField.m
+++ TauGame/AsteroidField.m
@@ -18,6 +18,7 @@
   if (self) {
     // Set up coordinates
     [self setLeft:0 right:8 bottom:0 top:12];
+    [self orientationChangedTo:UIDeviceOrientationLandscapeLeft];
     
     // Set up background
     clearColor = GLKVector4Make(0, 0, 0, 1);
@@ -79,13 +80,33 @@
 
 -(void)addRandomBaddie {
   Baddie *baddie = [[Baddie alloc] init];
+
+//  srand(arc4random());
   
   float randX = ((float)rand()/RAND_MAX) * self.visibleWidth + self.bottomLeftVisible.x;
   float randY = ((float)rand()/RAND_MAX) * (self.visibleHeight - 3) + self.bottomLeftVisible.y + 3;
   float randVelocity = -10.0 + (float)rand()/RAND_MAX * 20.0;
+  NSLog(@"y is %f", randY);
   baddie.position = GLKVector2Make(randX, randY);
   baddie.velocity = GLKVector2Make(randVelocity,0);
   baddie.shape.color = GLKVector4Make(((float)rand()/RAND_MAX), ((float)rand()/RAND_MAX), ((float)rand()/RAND_MAX), 1.0);
+  
+  if ((float)rand()/RAND_MAX > 0.8) {
+    TEScaleAnimation *grow = [[TEScaleAnimation alloc] init];
+    grow.scale = 1.2;
+    grow.duration = 1;
+    grow.repeat = TEAnimationRepeatForever;
+    [baddie.currentAnimations addObject:grow];
+  }
+  
+  if ((float)rand()/RAND_MAX > 0.8) {
+    TERotateAnimation *spin = [[TERotateAnimation alloc] init];
+    spin.rotation = 1*M_TAU;
+    spin.duration = 1;
+    spin.repeat = TEAnimationRepeatForever;
+    [baddie.currentAnimations addObject:spin];
+  }
+  
   [self.characters addObject:baddie];
   [ships addObject:baddie];
 }
